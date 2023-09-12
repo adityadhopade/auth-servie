4d45ee23135ee36e74825aef2a3d636df2e5d648
adityadhopade <adityadhopade18@gmail.com>
2023-09-12 16:15:28 +0530

    [enhamcement]add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { HttpError } from 'http-errors';
+import { logger } from './config/logger';
 const app = express();
+
 app.use(express.json());
 
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Welocme to the api-suth server');
+    res.send('Welcome to the api-auth server');
+});
+
+app.use((err: HttpError, req: Request, res: Response) => {
+    logger.error(err.message);
+    res.status(err.statusCode).json({
+        name: err.name,
+        statusCode: err.statusCode,
+        message: err.message,
+        details: [],
+    });
 });
 
 export default app;
